793515b41f0b806ba83b060e92d047ce2cecd11e
Ayush Patel <ayushhpatel.2354@gmail.com>
2026-05-24 22:41:07 +0530

    chore: initial setup with Next.js, Tailwind, and Prisma

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className = "flex flex-col h-screen justify-center items-center">
+      <h1 className="text-4xl text-green-500 font-extrabold">Hello World 👋🏻👋🏻</h1>
+    </div>
+  );
+}
